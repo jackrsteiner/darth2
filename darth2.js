429dfd9e6bd192d822aa081ad661fb9d4ba582e9
jackrsteiner <jackrsteiner@gmail.com>
2014-11-12 10:59:42 +1100

    add up and downvote event handlers

--- darth2.js
+++ darth2.js
@@ -9,21 +9,31 @@ if (Meteor.isClient) {
   });
   
   Template.body.events({
-  "submit .new-idea": function (event) {
-    // This function is called when the new task form is submitted
+    "submit .new-idea": function (event) {
+      // This function is called when the new task form is submitted
 
-    var text = event.target.text.value;
+      var text = event.target.text.value;
 
-    Ideas.insert({
-      text: text,
-      createdAt: new Date() // current time
-    });
+      Ideas.insert({
+        text: text,
+        createdAt: new Date(), // current time
+        score: 1
+      });
 
-    // Clear form
-    event.target.text.value = "";
+      // Clear form
+      event.target.text.value = "";
 
-    // Prevent default form submit
-    return false;
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+  Template.individual_ideas.events({
+  "click .upvote": function () {
+    Ideas.update(this._id, {$inc: {score: 1}});
+  },
+  "click .downvote": function () {
+    Ideas.update(this._id, {$inc: {score: -1}});
   }
 });
 }
